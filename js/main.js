21830262534abce0359112138efaa21d9f10675d
IXDCOZA <dylan@ixd.co.za>
2017-08-30 17:31:02 +0200

    screensaver page

--- js/main.js
+++ js/main.js
@@ -2,13 +2,41 @@ $( document ).ready(function() {
   $(function() {
 	   FastClick.attach(document.body);
   });
-  $(function(){
-    x = $.webScreenSaver({
-      stagnantDelay: 60000, // 1 minute
-      videos: [{
-        mp4: './video/screensaver.mp4'
-      }]
-    });
+
+  //Increment the idle time counter every minute.
+  var idleInterval = setInterval(timerIncrement, 1000); // 1 minute
+
+  //Zero the idle timer on mouse movement.
+  $(this).mousemove(function (e) {
+      idleTime = 0;
+  });
+  $(this).keydown(function (e) {
+      idleTime = 0;
+  });
+  $(this).mousedown(function (e) {
+      idleTime = 0;
+  });
+  $(this).click(function (e) {
+      idleTime = 0;
+  });
+  $(this).scroll(function (e) {
+      idleTime = 0;
   });
 
+  $(document).on('touchstart click', this, function(e){
+      e.stopPropagation(); //stops 'ghost clicks' (double clicking)
+      idleTime = 0;
+    });
+
+
   }); // end document ready
+
+
+  var idleTime = 0;
+
+  function timerIncrement() {
+      idleTime = idleTime + 1;
+      if (idleTime > 60) { // 1 minute
+          window.location.href="./screensaver.html"
+      }
+  }
